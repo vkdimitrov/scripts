95a2133c4749ab8cfe770e99c7a294dd552b677f
Vladimir <dimitrov@vladko.org>
2014-01-09 16:43:37 +0200

    Update webfetch.sh
    hide credentials

--- webfetch.sh
+++ webfetch.sh
@@ -20,33 +20,9 @@ else
         rm -rf /home/git/production/repos/$1
         #setvame env promenlivata @site
         case "$1" in
-        factor)
-                        site="root@factoring-bg.com:/var/www/factorin-bg.com-deploy"
+        factoaar)
+                        site="xxxx@xx.xx.xx.xx:/var/www/"
                         ports='ssh -p11122'
-                    ;;
-        br3)
-                        site="bridgebg@bridge.bg:/home/bridgebg/public_html"
-                        ports='ssh -p1022'
-                    ;;
-        ues)
-                        site="uniqp1jx@uniqueestates.net:/home/uniqp1jx/public_html"
-                        ports='ssh -p1022'
-                    ;;
-        sms-reminder)
-                        site="bridgebg@bridge.bg:/home/bridgebg/smsreminder.bg"
-                        ports='ssh -p1022'
-                    ;;
-        geograf)
-                        site="geografb@geograf.bg:/home/geografb/public_html"
-                        ports='ssh -p1022'
-                ;;
-        ifg)
-                        site="ifgleasingssh@ifgleasing.eu:/var/www/clients/client3/web4/test"
-                        ports='ssh -p10022'
-                    ;;
-        voltron)
-                        site="vkdimitrov@vladko.org:/home/vkdimitrov/public_html/voltron"
-                        ports='ssh -p229'
                     ;;
                *) echo "nqma takova repo" exit
                     ;;

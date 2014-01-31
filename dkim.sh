f886d8202f8552cb0e2e0a09bae89f8e256f4786
Vladimir <dimitrov@vladko.org>
2014-01-31 15:21:13 +0200

    Update dkim.sh
    write rules to table files

--- dkim.sh
+++ dkim.sh
@@ -7,5 +7,8 @@ opendkim-genkey -t -s mail -d $1
 mv mail.private mail
 chown opendkim:opendkim mail mail.txt
 chmod u=rw,go-rwx mail mail.txt
+echo $1" $1:mail:/etc/opendkim/$1/mail">>/etc/opendkim/KeyTable
+echo "*@$1          $1">>/etc/opendkim/SigningTable
+
 cat /etc/opendkim/$1/mail.txt
 /etc/init.d/opendkim restart

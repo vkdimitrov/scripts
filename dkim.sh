fae4fd7feae3628d9a0addbb40872476780489a6
Vladimir <dimitrov@vladko.org>
2014-02-12 12:10:31 +0200

    Update dkim.sh
    dispaly dns help

--- dkim.sh
+++ dkim.sh
@@ -9,6 +9,8 @@ chown opendkim:opendkim mail mail.txt
 chmod u=rw,go-rwx mail mail.txt
 echo $1" $1:mail:/etc/opendkim/$1/mail">>/etc/opendkim/KeyTable
 echo "*@$1          $1">>/etc/opendkim/SigningTable
+echo $1 "TXT    v=spf1 mx -all"
+echo "_dmarc.$1 TXT v=DMARC1; p=reject; pct=100; rua=mailto:dmarc@vladko.org; ruf=mailto:dmarc@vladko.org"
 
 cat /etc/opendkim/$1/mail.txt
 /etc/init.d/opendkim restart
